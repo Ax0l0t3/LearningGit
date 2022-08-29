cc3c76d27ddee37a8d84e54c97154f6d6c7407d4
D4v3 <antenopolis@gmail.com>
2022-08-28 20:12:58 -0500

    Modal Dropdown style

--- src/Components/modalComponent.js
+++ src/Components/modalComponent.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDom from 'react-dom';
 import '../Styles/modalComponent.css';
 
 export function Modal ({ modalStatus, handleCloseProp, modalTaskProp }){
+  const [dropdown, setDropdown] = useState(false);
+  
+  const dropdownOpen = () => dropdown === true ? setDropdown(false) : setDropdown(true);
+  const dropdownClose = () => { handleCloseProp(); setDropdown(false) }
+  
+  const progressList = [
+    "To Do",
+    "In Progress",
+    "In Hold",
+    "Done"
+  ]
+  
   if(modalStatus === false) return;
 	return ReactDom.createPortal(
     <div className="backgrnd">
@@ -10,19 +22,29 @@ export function Modal ({ modalStatus, handleCloseProp, modalTaskProp }){
         <div className="modalHead">
           <h2 className="modalTitle">{modalTaskProp.value}</h2>
           <div className="modalList">
-            <p>{modalTaskProp.progress}</p>
-            {/*<ul>
-              <li>To Do</li>
-              <li>In Progress</li>
-              <li>In Hold</li>
-              <li>Done</li>
-            </ul>*/}
+            <p onClick={ dropdownOpen }>{modalTaskProp.progress}</p>
+              {
+                dropdown === true
+                ? <ul>
+                {
+                  progressList.map( (y, key) =>{
+                    if( y === modalTaskProp.progress) return <li className="selected" key={key}>{y}</li>
+                    return <li key={key}>{y}</li>
+                  })
+                    /*<li>To Do</li>
+                    <li>In Progress</li>
+                    <li>In Hold</li>
+                    <li>Done</li>*/
+                }
+                  </ul>
+                : null
+              }
           </div>
         </div>
         <input type="text" placeholder="Type your task description..." />
         <div className="modalFooter">
           <button type="button" className="saveButton">Save</button>
-          <button type="button" className="backButton" onClick={ handleCloseProp }>Back</button>
+          <button type="button" className="backButton" onClick={ dropdownClose }>Back</button>
         </div>
       </div>
     </div>,
