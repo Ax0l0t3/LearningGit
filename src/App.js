58951aa326ac5e878488bf687d156032c80eeb88
D4v3 <antenopolis@gmail.com>
2023-12-08 10:37:27 -0600

    Fixed Plus button style; Prettier changes

--- src/App.js
+++ src/App.js
@@ -1,86 +1,86 @@
-import React, { useState } from 'react';
-import CardList from './Components/CardListComponent';
-import PlusButtn from './Components/plusButtnComponent';
-import {Modal} from './Components/modalComponent';
-import './App.css';
+import React, { useState } from "react";
+import CardList from "./Components/CardListComponent";
+import PlusButtn from "./Components/plusButtnComponent";
+import { Modal } from "./Components/modalComponent";
+import "./App.css";
 
 function App() {
-  
-  const [task, setTask] = useState ('');
+  const [task, setTask] = useState("");
   const [taskArray, setTaskArray] = useState([]);
   const [openModal, setOpenModal] = useState(false);
-  const [modalTask, setModalTask] = useState('');
+  const [modalTask, setModalTask] = useState("");
   const [readedValue, setReadedValue] = useState(false);
-  const cardHeader = [
-    "Tasks To Do",
-    "In Progress",
-    "In Hold",
-    "Tasks Done"
-  ]
-  
-  const handleChange = x => {
-    setTask( x.target.value );
+  const cardHeader = ["Tasks To Do", "In Progress", "In Hold", "Tasks Done"];
+
+  const handleChange = (x) => {
+    setTask(x.target.value);
   };
-  
+
   const handleClick = () => {
-    if(task === '')return;
+    if (task === "") return;
     const getRandomIntInclusive = (min, max) => {
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
     };
     const taskObject = {
-      id: `${ task.length }-${ task.slice(0,3) }-${ getRandomIntInclusive( 0, task.length )}-${ getRandomIntInclusive( 1, task.length ) }`,
+      id: `${task.length}-${task.slice(0, 3)}-${getRandomIntInclusive(
+        0,
+        task.length,
+      )}-${getRandomIntInclusive(1, task.length)}`,
       value: task,
-      progress: 'To Do',
-      readed: readedValue
+      progress: "To Do",
+      readed: readedValue,
     };
-    setTaskArray( [...taskArray, taskObject] );
-    setTask('');
+    setTaskArray([...taskArray, taskObject]);
+    setTask("");
   };
 
-  const handleOpen = modalTask => {
+  const handleOpen = (modalTask) => {
     setOpenModal(true);
     setModalTask(modalTask);
-  }
+  };
   const handleClose = () => setOpenModal(false);
-  
+
   return (
     <div className="App">
       <div className="Titles">
         <h1>Tasks</h1>
         <div className="inputDiv">
-          <input className="inputTask" type="text" placeholder="Type Your Task..." onChange={ handleChange } value={ task }/>
-          <div onClick={ handleClick } >
+          <input
+            className="inputTask"
+            type="text"
+            placeholder="Type Your Task..."
+            onChange={handleChange}
+            value={task}
+          />
+          <div style={{ display: "flex" }} onClick={handleClick}>
             <PlusButtn />
           </div>
         </div>
         <p className="tradeMark">TL4K4® 2022©</p>
       </div>
-	    <Modal
+      <Modal
         modalStatus={openModal}
-        handleCloseProp={ handleClose }
-        modalTaskProp={ modalTask }
+        handleCloseProp={handleClose}
+        modalTaskProp={modalTask}
       />
       <div className="Cards">
-      {
-        cardHeader.map( (header, keyHeader) => (
+        {cardHeader.map((header, keyHeader) => (
           <CardList
-            key = {keyHeader}
-            columnKey = {keyHeader}
-            taskReaded = {setReadedValue}
-            headerTitle = {header}
-            valuesList = {taskArray}
-            setValuesList = {setTaskArray}
-            modalStatus = { openModal }
-            handleOpenProp={ handleOpen }
+            key={keyHeader}
+            columnKey={keyHeader}
+            taskReaded={setReadedValue}
+            headerTitle={header}
+            valuesList={taskArray}
+            setValuesList={setTaskArray}
+            modalStatus={openModal}
+            handleOpenProp={handleOpen}
           />
-        ))
-      }
+        ))}
       </div>
     </div>
   );
 }
 
 export default App;
-
